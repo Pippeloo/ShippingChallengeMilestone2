b8b7da762cbbff6b4200818aabad5b0a65417977
Pippeloo <julestorfs@amps.be>
2022-12-04 15:25:43 +0100

    Added hostname to the page

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from .classes.Database import Database
+import os
 
 db = Database(
     host="scm2-db-jt",
@@ -20,3 +21,7 @@ def read_status():
 @app.get("/api/user")
 def read_user():
     return db.get_user()
+
+@app.get("/api/hostname")
+def read_hostname():
+    return {"hostname": os.uname()[1]}
